afbf6105a2000f357d2cdedb0f7d39739eae50e2
Raul Doe <khoado@csu.fullerton.edu>
2015-12-01 16:41:18 -0800

    my test from laptop

--- HCApp/App_Start/BundleConfig.cs
+++ HCApp/App_Start/BundleConfig.cs
@@ -3,7 +3,7 @@ using System.Web.Optimization;
 
 namespace HCApp
 {
-    //test
+    //test from laptop
     public class BundleConfig
     {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
