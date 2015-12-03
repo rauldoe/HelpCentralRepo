f1920de502eb792ce35591740fa4ef07f7e5d132
ifazil786 <ifazil786@gmail.com>
2015-12-02 16:01:09 -0800

    test!!! worked on schedulerController.cs

---
+++ HCApp/Controllers/schedulersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HCApp.Controllers
+{
+    public class schedulersController : Controller
+    {
+        private helpcentraldbEntities db = new helpcentraldbEntities();
+
+        [HttpPost]
+        public ActionResult Login(scheduler s)
+        {
+            string actionAfterLogin = "LoginBad";
+            ViewResult vr = View(actionAfterLogin);
+
+            if (ModelState.IsValid)
+            {
+                var schedulerList = db.schedulers.ToList();
+
+                var userToLogin = db.schedulers.Where(i => (i.email == s.email) && (i.password == s.password)).FirstOrDefault();
+
+                if (userToLogin != null)
+                {
+                    actionAfterLogin = "LoginGood";
+                    vr = View(actionAfterLogin);
+                }
+            }
+
+            return vr;
+        }
+        // GET: schedulers
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
