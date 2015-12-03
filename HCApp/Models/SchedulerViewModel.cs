65dd207ed972cb2608d4b1a8a369a58c2cff6374
rauldoe <khoado@csu.fullerton.edu>
2015-12-02 16:00:21 -0800

    Update on view model for the scheduler

---
+++ HCApp/Models/SchedulerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HCApp.Models
+{
+    public class ScheulerViewModel
+    {
+        public List<CustomerViewModel> CustomerViewModelList { get; set; }
+        public List<GenieViewModel> GenieViewModelList { get; set; }
+        
+    }
+}
